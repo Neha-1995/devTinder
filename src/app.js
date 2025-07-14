8820f044f5fe4457df84f73b59cd560d1b86416e
Neha-1995 <vermanehcse@gmail.com>
2025-07-14 13:39:23 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -2,11 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res) =>{
-    res.send("Hello From the server");
+app.get("/user",(req,res) => {
+     res.send({firstName: "Kumari Neha", lastName: "Verma"});
+});
+
+app.post("/user",(req,res) =>{
+    res.send("data saved successfully in db ");
 })
 
-app.listen(3000 ,() => {
-    console.log('server is running on port 3000');
+app.delete("/user",(req,res) =>{
+    res.send("data deleted successfully ");
+})
+
+
+app.use("/Test",(req,res) =>{
+    res.send("Hello From the test  server");
+})
+
+app.listen(7777 ,() => {
+    console.log('server is running on port 7777');
 }
 );
